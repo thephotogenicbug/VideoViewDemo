076f7d4c61da14e3815999655180c249582f1ba9
thephotogenicbug <navinkumarachari@gmail.com>
2022-09-10 22:04:44 +0530

    Initial commit

---
+++ app/src/main/java/com/naveen/videoviewdemo/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.naveen.videoviewdemo
+
+import android.net.Uri
+import android.net.Uri.parse
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
+import java.net.URI
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+       
+    }
+}
